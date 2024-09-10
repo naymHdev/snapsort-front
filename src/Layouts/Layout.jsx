fb126632183fd1c671de3d53734c0971a6e7007c
MacBook Pro <naym100m@gmail.com>
2024-09-10 17:31:32 +0600

    [created] a ready mate footer

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer";
+import NavBar from "../Shared/NavBar";
 
 const Layout = () => {
   return (
     <div>
-      <Outlet />
+      <NavBar />
+      <div className=" min-h-screen overflow-hidden">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
---
+++ src/Shared/Footer.jsx
@@ -0,0 +1,35 @@
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-gray-800 text-white py-6">
+        <div className="container mx-auto text-center">
+          <h2 className="text-lg font-semibold mb-2">Snap Sort</h2>
+          <p className="mb-4">
+            Showcasing the best moments in a beautiful and organized gallery.
+          </p>
+          <div className="flex justify-center space-x-4 mb-4">
+            <FaFacebook />
+            <FaTwitter />
+            <FaLinkedin />
+            <FaInstagram />
+            <FaYoutube />
+          </div>
+          <p className="text-sm text-gray-400">
+            &copy; {new Date().getFullYear()} Image Gallery. All rights
+            reserved.
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
